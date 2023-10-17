d54cd73ebba20dada9b9a7a7133dcaa2f6e8aee6
Eli Wang <eliwang1@gmail.com>
2023-10-17 15:42:15 -0400

    Add files via upload

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -11,13 +11,29 @@ builder.Services.AddDbContext<StoreDbContext>(opts => {
 });
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession();
+
+app.MapControllerRoute("catpage",
+    "{category}/Page{productPage:int}",
+    new { Controller = "Home", action = "Index" });
+
+app.MapControllerRoute("page", "Page{productPage:int}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("category", "{category}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapControllerRoute("pagination",
     "Products/Page{productPage}",
     new { Controller = "Home", action = "Index" });
+
 app.MapDefaultControllerRoute();
 
 // SeedData.EnsurePopulated(app);
